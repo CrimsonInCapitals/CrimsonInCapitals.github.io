378fd860726d5c433aa558793682f2560eb5503b
CrimsonInCapitals <harrison.cole@crimsonincapitals.com>
2023-03-02 17:23:57 +0000

    fixing links map

--- reactportfolio/crimson/src/App.js
+++ reactportfolio/crimson/src/App.js
@@ -2,41 +2,36 @@ import './App.css';
 import {Link,Routes,Route} from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import React from 'react';
+import Home from './pages/home';
 
-function App() {
-  const hello = '11'
-  const {webpages,setWebPages} = useState()
-  setWebPages({
-    home: {
-      name: 'Home',
-      url:'/',
-      component: 'home'
-    },
-    contact: {
-      name:'Contact',
-      url:'/contact',
-      component: 'get in touch'
+const App = ()=> {
+  class Page {
+    constructor(name='',url='/',component=<Home/>){
+      this.name = name
+      this.url = url
+      this.component = component
     }
-  })
-  useEffect(()=>{console.log(webpages)},[webpages]);
-  console.log(hello)
+  }
+  const pages = [
+    new Page('Home','/',<Home/>),
+    new Page('Contact','/contact',<Home/>),
+    new Page('About','/about',<Home/>)
+]
   return (
     <>
-    <header>
-      <nav>
-        {webpages.map(({name,url,component})=>(<Link to={url}> {name} </Link>))}
-      </nav>
-    </header>
-    <main>
-      <Routes>
-        <Route path='/' element={'home'}/>
-        <Route path='/contact' element={'get in touch'}/>
-      </Routes>
-
-    </main>
-    <footer>
-      footer
-    </footer>
+      <header>
+        <nav>
+          {pages.map(({name,url,component})=>(<Link key={url} to={url}> {name} </Link>))}
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          {pages.map(({name,url,component})=>(<Route key={url} path={url} element={component}/>))}
+        </Routes>
+      </main>
+      <footer>
+        footer
+      </footer>
     </>
     );
 }
---
+++ reactportfolio/crimson/src/pages/home.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import {Link,Routes,Route} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import React from 'react';
+
+const Home = ()=> {
+    return (
+    <section>
+        <h1>home page</h1>
+    </section>
+    );
+}
+
+export default Home;
