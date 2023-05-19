f60441df16749a1c34b20ba3ba22077b127a368b
CrimsonInCapitals <harrison.cole@crimsonincapitals.com>
2023-05-19 11:23:47 +0100

    casestudie card borders and themed text

--- crimson/src/components/casecard.js
+++ crimson/src/components/casecard.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { H1, H2, P } from './StyledComponents';
+import { CH1, H1, H2, P } from './StyledComponents';
+import { useThemeContext } from '../context/theme';
 export const CaseCard = ({Header='Default',Image=require('../sources/default.png'),size='small',To})=> {
+    const theme = useThemeContext()
+    const [cardStyle,setCardStyle] =useState({backgroundColor: theme.Card.Background,border: theme.Card.Accent+' 2px solid'})
+    useEffect(()=>{
+        setCardStyle({backgroundColor: theme.Card.Background,border:theme.Card.Accent+' 2px solid'})
+    },[theme])
 return(
     To ?
-    <Link className={size+'Card card'} to={To}>
+    <Link className={size+'Card card'} to={To} style={cardStyle}>
         <img src={Image} alt={Header}/>
-        <h2 className='cardheader'>{Header}</h2>
+        <CH1 className='cardheader'>{Header}</CH1>
 
     </Link>:
-    <article className={size+'Card card'}>
+    <article className={size+'Card card'} style={cardStyle}>
     <img src={Image} alt={Header}/>
-    <h2 className='cardheader'>{Header}</h2>
+    <CH1 className='cardheader'>{Header}</CH1>
 
 </article>
     )
@@ -27,11 +33,16 @@ return(
 }
 
 export const CaseWithAside = ({Header='Default',Image=require('../sources/default.png'),size='small',To,children,Right=true})=> {
+    const theme = useThemeContext()
+    const [cardStyle,setCardStyle] =useState({backgroundColor: theme.Card.Background,border: theme.Card.Accent+' 2px solid'})
+    useEffect(()=>{
+        setCardStyle({backgroundColor: theme.Card.Background,border:theme.Card.Accent+' 2px solid'})
+    },[theme])
     return(
         <article className={size+'Card card cardwithaside'}>
             {!Right&& children &&  <P>{children}</P>}
 
-            <img src={Image} alt={Header} className='withaside'/>
+            <img src={Image} alt={Header} style={cardStyle}className='withaside'/>
 
             {Right&& children &&  <P>{children}</P>}
         </article>
