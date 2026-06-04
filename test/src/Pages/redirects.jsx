cd4bdb4c8fb0192f54ed1116e6564bc3dfbfb115
CrimsonInCapitals <harrison.cole@crimsonincapitals.com>
2026-06-04 17:23:44 +0100

    new build move article

--- test/src/Pages/redirects.jsx
+++ test/src/Pages/redirects.jsx
@@ -6,7 +6,7 @@ export const GLCQR260003 = new RedirectClass('Villas','/GLC-QR-260003',Pages.TGL
 export const GLCQR260001 = new RedirectClass('Capabilities','/GLC-QR-260001',Pages.TGLCLinks('route'))
 export const GLCQR260004 = new RedirectClass('STL','/GLC-QR-260004',Pages.TGLCLinks('route'))
 export const GLCQR260005 = new RedirectClass('test','/GLC-QR-260005','https://thegloballivingcompany.com/pages/contact',true)
-export const CRMQR260001 = new RedirectClass('afmockups','/articles/af-createmockups.html',Pages.createmockupsaf('route'))
+export const CRMQR260001 = new RedirectClass('afmockups','/af-createmockups.html',Pages.createmockupsaf('route'))
 
 
 
